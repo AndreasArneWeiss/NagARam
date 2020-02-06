5d70427aed6145c31beabee6ea7483be909f7169
AndreasArneWeiss <mightyray564@gmail.com>
2020-02-06 13:37:49 +0200

    Laravel Init

---
+++ storage/framework/views/ec49c08fc1de7d10df28370edadd94b519cffc4d.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Anagram</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
+<?php /**PATH /home/adven/PhpstormProjects/NagARam/resources/views/welcome.blade.php ENDPATH**/ ?>
